713dbcbd249e1d46eb54f2d9c88efe87c736d2ea
EliasS0p0rt3Web <SL71748619@idat.pe>
2026-07-21 17:03:03 -0500

    Entrega final EC3 microservicios dockerizados

--- ms-cita/src/main/java/com/cita/demo/client/MedicoClient.java
+++ ms-cita/src/main/java/com/cita/demo/client/MedicoClient.java
@@ -4,9 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "MedicoClient", url = "http://localhost:8083/api/v1/horarios-medicos")
+@FeignClient(
+        name = "MedicoClient",
+        url = "${MEDICO_URL:http://localhost:8083/api/v1/horarios-medicos}"
+)
 public interface MedicoClient {
 
     @GetMapping("/validar-turno")
-    boolean validarTurno(@RequestParam("colegiatura") String colegiatura, @RequestParam("turno") String turno);
+    boolean validarTurno(
+            @RequestParam("colegiatura") String colegiatura,
+            @RequestParam("turno") String turno
+    );
 }
